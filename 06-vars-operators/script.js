fbea6a7a64d7460656f5114cac29e97c38379ac5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-08-19 20:12:14 +0600

    Add lesson JS: vars and operators

---
+++ 06-vars-operators/script.js
@@ -0,0 +1,14 @@
+console.log(23 + 97);
+console.log(10 + 45 + 32 + 5 + 9 + 100);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage);
