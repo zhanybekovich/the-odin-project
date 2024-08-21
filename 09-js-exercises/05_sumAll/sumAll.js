31aac5a2b7ecc169483223f035f3f3b0c097eea2
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-08-21 10:57:00 +0600

    Add JS ex. 5

---
+++ 09-js-exercises/05_sumAll/sumAll.js
@@ -0,0 +1,12 @@
+const sumAll = function (start, end) {
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+// Do not edit below this line
+module.exports = sumAll;
