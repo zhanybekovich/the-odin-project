d92a23a4647c66fe243f6adf7cc7e300a30a0685
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-08-22 17:56:58 +0600

    Add JS ex. 8

---
+++ 09-js-exercises/08_calculator/calculator.js
@@ -0,0 +1,34 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (...args) {
+  return args.reduce((a, b) => a + b, 0);
+};
+
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current);
+};
+
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
+
+const factorial = function (n) {
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
